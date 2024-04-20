47be8288eda6e7a7a030305fa8ee1f8ece0978d2
Minh Nhat <wunnynhatminh2602@gmail.com>
2024-04-20 22:29:33 +0700

    adapable

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 // import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb+srv://minhnguyendoannhat:nhatminh2602@moneymanager.ojakwin.mongodb.net/?retryWrites=true&w=majority&appName=moneyManager';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(databaseUrl)
     // UserModule
   ],
   controllers: [AppController],
