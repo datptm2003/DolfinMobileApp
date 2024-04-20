fbaeb4da49b43b12a16980ded996bc6786074a5a
Minh Nhat <wunnynhatminh2602@gmail.com>
2024-04-20 21:49:33 +0700

    adapable

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true
-    }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule
   ],
